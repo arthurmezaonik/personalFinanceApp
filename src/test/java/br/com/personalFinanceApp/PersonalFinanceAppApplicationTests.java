5e8ca4c456637aae274cd8a5d15fffd8772f211c
Arthur Martins <arthur.mezaonik@gmail.com>
2026-02-23 23:22:36 +0000

    first commit

---
+++ src/test/java/br/com/personalFinanceApp/PersonalFinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.personalFinanceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
